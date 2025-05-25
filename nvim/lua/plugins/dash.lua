79a42645366f47f61fd036381d04e7637845aa59
anas.elmoussaoui <anas.elmoussaoui@visier.com>
2025-05-25 21:10:48 +0200

    manipulation nvim

---
+++ nvim/lua/plugins/dash.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+
+      sections = {
+        -- {
+        --   section = "terminal",
+        --   cmd = "chafa ~/.config/nvim/assets/watania.png --format kitty --symbols vhalf --size 50x50; sleep .1",
+        -- },
+        -- { section = "terminal", cmd = "fortune -s | cowsay -dC", hl = "header", padding = 1, indent = 8 },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    },
+    picker = {
+      projects = {
+        dev = {
+          "~/source/",
+        },
+        projects = {
+          "~/.config",
+        },
+      },
+    },
+  },
+}
